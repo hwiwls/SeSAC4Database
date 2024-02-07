4f275436c137bb30db59378959bff1a121454bcb
hwiwls <80039139+hwiwls@users.noreply.github.com>
2024-02-07 09:21:30 +0900

    Initial Commit

---
+++ SeSAC4Database/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC4Database
+//
+//  Created by hwijinjeong on 2/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
